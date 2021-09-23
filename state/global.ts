0efbf3e76fcb7b297f00271402881c50d2cb3a83
Chris Kang <chris.v.kang@gmail.com>
2021-09-23 16:48:37 -0700

    added some global state

--- state/global.ts
+++ state/global.ts
@@ -4,11 +4,19 @@ import { GeoPosition } from 'react-native-geolocation-service';
 export type GlobalStateProps = {
   currentRun: Array<GeoPosition>;
   currentPauses: Array<Array<Number>>;
+  currentPace: Number;
+  averagePace: Number;
+  startTime: Number;
+  distance: Number;
 };
 
 const globalState = createState<GlobalStateProps>({
   currentRun: [],
   currentPauses: [],
+  currentPace: 0,
+  averagePace: 0,
+  startTime: 0,
+  distance: 0,
 });
 
 export default globalState;
